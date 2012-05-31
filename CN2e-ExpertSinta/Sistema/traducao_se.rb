51a8f6ffa33e212281fe6651931c3cf5c853b703
Chelder lins <chelder.lins@gmail.com>
2012-05-30 23:32:54 -0300

    Sistema concluído. Aplicando classes e refinando código Falta implementar classes

--- CN2e-ExpertSinta/Sistema/traducao_se.rb
+++ CN2e-ExpertSinta/Sistema/traducao_se.rb
@@ -1,6 +1,31 @@
 # encoding: UTF-8
 
 class TraducaoSe
+
+  regexs = Hash[ 'SE' => /SE/,
+               'E' => /\sE\s/,
+               'ENTAO' => /ENTAO/,
+               '=' => /=/,
+               'then' => /then/,
+               'end' => /end/,
+               'espaco' => /\s/,
+               'CNF' => /.\sCNF\s\d*%/,
+               'Regra' => /Regra\s\d*/,
+               'REGRAS' => /REGRAS/,
+               'PERG' => /PERGUNTAS/,
+               #regexp que encontra frases
+               'frases' => /(\w*\s?)*/,
+               'andesp' => /and\s+/,
+               'ifesp' => /if\s+/,
+               'thenesp' => /then\s+/,
+               'espif' => /\s+if/,
+               'espand' => /\s+and/,
+               'espthen' => /\s+then/,
+               'ultpalavra' => /= \w+$/,
+               '=esp' => /=\s/,
+               'variavel' => /\w+\s+\=/
+              ]
+
   # Remove as letras acentuadas
   def remover_acentos(texto)
     texto = texto.gsub(/[á|à|ã|â]/, 'a'.force_encoding("UTF-8")).gsub(/(é|è|ê)/, 'e').gsub(/(í|ì|î)/, 'i').gsub(/(ó|ò|õ|ô)/, 'o').gsub(/(ú|ù|û)/, 'u')
@@ -17,13 +42,53 @@ class TraducaoSe
   end
 
   #Encontra a última palavra de cada linha e coloca aspas duplas
-  def ultima_palavra(texto)
-    if !(linha =~ regexs['then']) and (linha =~ regexs['andesp'] or linha =~ regexs['ifesp'])
-      regexs['ultpalavra'].match(linha)
+  def ultima_palavra(linha)
+    if !(linha =~ $regexs['then']) and (linha =~ $regexs['andesp'] or linha =~ $regexs['ifesp'])
+      $regexs['ultpalavra'].match(linha)
       var = $&
-      var2 = var.gsub(regexs['=esp'], "")
+      var2 = var.gsub($regexs['=esp'], "")
       linha = linha.gsub("#{var}", "= \"#{var2}\"")
     end
+
+    return linha
+  end
+
+  #Remove o espaço em excesso que está no início das linhas
+  def remove_espaco_init(linha)
+    if linha =~ $regexs['ifesp'] or linha =~ $regexs['andesp'] or linha =~ $regexs['thenesp']
+      linha = linha.gsub($regexs['espif'], "if").gsub($regexs['espand'], "and").gsub($regexs['espthen'], "then")
+    end
+
+    return linha
+  end
+
+  #Encontra as variáveis e substitui os espaços entre elas pelo underline
+  def altera_variavel(linha)
+    if linha =~ $regexs['ifesp'] or linha =~ $regexs['andesp'] or linha =~ $regexs['thenesp']
+      init_variavel = $'
+
+      if init_variavel =~ /\s*=/
+        variavel = $`
+        variavelmod = variavel.gsub(/\s/, "_")
+      end
+    end
+
+    return linha
+  end
+
+  #Encontra o valor na linha do then e coloca-o entre aspas duplas
+  def aspas_valor(linha)
+    if linha =~ $regexs['then']
+      $regexs['=esp'].match(linha)
+      init_valor = $'
+
+      if init_valor =~ /\send/
+        valor = $`
+        valormod = valor.gsub("#{valor}", "\"#{valor}\"")
+      end
+    end
+
+    return linha
   end
 end
 
